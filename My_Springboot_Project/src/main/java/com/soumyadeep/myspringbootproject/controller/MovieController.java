df9379133477e407a92e474fa66c5bcd9abb030b
Soumyadeep Roy <soumyadeep.sdp2000@gmail.com>
2022-09-13 22:23:50 +0530

    3 rd commit

--- My_Springboot_Project/src/main/java/com/soumyadeep/myspringbootproject/controller/MovieController.java
+++ My_Springboot_Project/src/main/java/com/soumyadeep/myspringbootproject/controller/MovieController.java
@@ -1,7 +1,10 @@
 package com.soumyadeep.myspringbootproject.controller;
 
 import com.soumyadeep.myspringbootproject.dto.AddMovieDto;
+import com.soumyadeep.myspringbootproject.dto.MovieDto;
+import com.soumyadeep.myspringbootproject.dto.ReviewMovieDto;
 import com.soumyadeep.myspringbootproject.entity.Genre;
+import com.soumyadeep.myspringbootproject.entity.Movie;
 import com.soumyadeep.myspringbootproject.entity.Language;
 import com.soumyadeep.myspringbootproject.service.MovieService;
 import com.soumyadeep.myspringbootproject.service.UtilityService;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
@@ -37,7 +41,7 @@ public class MovieController {
             return utilityService.sendErrorResponse("Invalid Request", result.get("errors"), HttpStatus.BAD_REQUEST.value());
         }
         movieService.addMovie(data);
-        return utilityService.sendSuccessResponse(data);
+        return utilityService.sendSuccessResponse("Movie Added Successfully!!!",null);
     }
 
     @GetMapping("/all-genres")
@@ -49,6 +53,30 @@ public class MovieController {
     @GetMapping("/all-languages")
     public ResponseEntity getAllLanguages(){
         List<Language> data = movieService.gatAllLanguages();
+        return utilityService.sendSuccessResponse(data);
+    }
+
+    @GetMapping("/review-movie")
+    public String reviewMovie(){
+        return "review-movie";
+    }
+
+    @PostMapping("/review-movie")
+    public ResponseEntity reviewMovie(@RequestBody ReviewMovieDto data){
+        HashMap<String, Object> result = movieService.validateReviewMovieRequest(data);
+        if(result.get("result").equals("0")){
+            return utilityService.sendErrorResponse("Invalid Request", result.get("errors"), HttpStatus.BAD_REQUEST.value());
+        }
+        movieService.reviewMovie(data);
+        return utilityService.sendSuccessResponse("Review Given Successfully!!!",null);
+    }
+
+    @GetMapping("/get-movies")
+    public ResponseEntity getMovies(@RequestParam(required = false) Long genre, @RequestParam(required = false) Long language){
+        if(genre == null && language == null){
+            return utilityService.sendErrorResponse("Please provide at least any one of genre or language", null, HttpStatus.BAD_REQUEST.value());
+        }
+        List<MovieDto> data = movieService.getMovies(genre, language);
         return utilityService.sendSuccessResponse(data);
     }
 
